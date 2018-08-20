a307b0836b2d49152bac86cb1abdf2140ae907ce
ghanendras <ghanendra shrivastava>
2018-08-20 18:40:18 +0530

    updated file

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+class ByeWorld{
+
+public static void main(String[]args)
+
+{
+System.out.println("Bye from Java");
+
+}
+}
---
+++ MyClass.java
@@ -0,0 +1,15 @@
+ public class MyClass
+{
+   public static void main(String[]args)
+    {System.out.println("My Class");
+}
+}
+
+class YourClass
+{
+public static void main(String[]args)
+{
+System.out.println("YourClass");
+}
+
+}
